93471de24ed4c61cb259c5e825ab0ae5c332fd73
nenadV91 <nenadwebdevelop@gmail.com>
2022-05-22 21:04:21 +0200

    Throw errors if env vars are missing

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -8,6 +8,16 @@ require("dotenv").config();
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
+if (!RINKEBY_PRIVATE_KEY) {
+  throw new Error(
+    "Please provide your private Rinkeby account key in the .env file"
+  );
+}
+
+if (!ALCHEMY_API_KEY) {
+  throw new error("Please create and provide Alchemy rinkeby api key");
+}
+
 module.exports = {
   solidity: "0.8.0",
   networks: {
